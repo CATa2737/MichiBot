37473b544a5efb51d20faacacd9ad629dca8b4c9
PrintfDead <ripprintf@gmail.com>
2022-09-03 17:17:09 -0300

    Fix update.js

--- city/bin/update.js
+++ city/bin/update.js
@@ -6,12 +6,13 @@ var database = mysql.createConnection({
         password: "",
         database: "michibot"
 })
+database.connect();
 
 module.exports = () => {
     city.houses.map(house => {
         let query1= `UPDATE houses`
-        let query2= `SET owner_name=${house.owner.name}, owner_id=${house.owner.id}`
-        let query3= `ẀHERE owner_id=${house.owner.id}`
+        let query2= `SET owner_name = '${house.owner.name}', owner_id = '${house.owner.id}'`
+        let query3= `ẀHERE owner_id = '${house.owner.id}'`
         let query = `${query1} ${query2} ${query3}`
         //Ignorá esta truchada, es mas facil leerlo, luego se actualiza
         //database.query()
