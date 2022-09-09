24f845eedb5ae84df18c6efffd05ec46a3fc2ca7
Adriel <danielaulloa.000@gmail.com>
2022-09-09 16:42:20 -0300

    Se indentó correctamente

--- update.js
+++ update.js
@@ -5,19 +5,16 @@ const versiones = new db.crearDB("updates");
 const memo = new db.memoDB("memo");
 
 exports.run = async(message) => {
-    
-  try {
-    if (!memo.has(`${message.guild.id}`)){
-    let version = await versiones.get(`${message.guild.id}`);
-    memo.set(`${message.guild.id}`, pack.version);
-
-    if (version !== pack.version) {
-      message.channel.send("🤖**EL BOT SE HA ACTUALIZADO EN ESTE SERVIDOR**🔴 🟡 🟢\n`si desea ver los cambios puede escribir /changes`")
-      versiones.set(`${message.guild.id}`, pack.version)
-    }
-    }
-  } catch (e) {
-    console.log(e.toString())
-      }
-  
+	try {
+    		if (!memo.has(`${message.guild.id}`)){
+    			let version = await versiones.get(`${message.guild.id}`);
+    			memo.set(`${message.guild.id}`, pack.version);
+    			if (version !== pack.version) {
+				message.channel.send("🤖**EL BOT SE HA ACTUALIZADO EN ESTE SERVIDOR**🔴 🟡 🟢\n`si desea ver los cambios puede escribir /changes`")
+      				versiones.set(`${message.guild.id}`, pack.version)
+    			}
+    		}
+	} catch (e) {
+    		console.log(e.toString())
+      	}
 }
